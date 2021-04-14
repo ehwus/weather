0e85bfe4e8b89b0cef729f21c2b52a26b2a02e38
Alex Withington-Smith <alex@awithsmith.com>
2021-04-14 09:49:47 +0100

    weather now displays on a very basic level on page

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import Weather from './weather';
 
 const container = document.getElementById('container');
 
-container.innerText = 'Hello from Typescript!';
-Weather.getWeather().then((response) => {
-  console.log(response);
+Weather.getCurrentWeather().then((response) => {
+  container.innerText = `You are in ${response.location}, where it is ${response.temperatureInKelvin} degrees Kelvin. ${response.headline}`;
 });
--- src/weather.ts
+++ src/weather.ts
@@ -1,10 +1,24 @@
+import { IWeather } from './types';
+
 export default class Weather {
-  static async getWeather() {
+  constructor(
+    public location: string,
+    public temperatureInKelvin: number,
+    public headline: string
+  ) {}
+
+  static async getCurrentWeather() {
     const response = await fetch(
       'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=61994fd9117fcb793efcd1c61bbcc70c',
       { mode: 'cors' }
-    );
+    ).then();
 
-    return await response.json();
+    const parsedResponse: IWeather = await response.json();
+
+    return new Weather(
+      parsedResponse.name,
+      parsedResponse.main.temp,
+      parsedResponse.weather[0].main
+    );
   }
 }
