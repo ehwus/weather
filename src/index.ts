a11fa8331fb7c1fbae30a04496b76a997d080ce0
Alex Withington-Smith <alex@awithsmith.com>
2021-04-15 08:02:57 +0100

    created box for results

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,18 @@ import './style.css';
 
 const gifLocationInDom = document.getElementById('gif');
 const input = document.getElementById('locationInput') as HTMLInputElement;
+const card = document.getElementById('resultsCard');
 const submit = document.getElementById('locationSubmit');
 const results = document.getElementById('results');
 
 submit.addEventListener('click', async () => {
   try {
+    card.style.visibility = 'hidden';
     const weather = await Weather.getCurrentWeather(input.value);
     results.innerText = `You are in ${weather.location}, where it is ${weather.temperature}°c. I would describe the weather as - ${weather.description}`;
     const gif = await Gif.getGif(weather.description);
-    gifLocationInDom.innerHTML = `<img src="${gif.url}" />`;
+    gifLocationInDom.innerHTML = `<img src="${gif.url} class=\"gif\"" />`;
+    card.style.visibility = 'visible';
   } catch {
     results.innerText = 'Location not found, please try again';
   }
