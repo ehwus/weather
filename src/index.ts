ac2f842151b6812f46c5c221f4b8e674d2c4b9e5
Alex Withington-Smith <alex@awithsmith.com>
2021-04-15 08:28:22 +0100

    fixed issue with image sizing

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,11 @@ submit.addEventListener('click', async () => {
     card.style.visibility = 'hidden';
     errorCard.style.visibility = 'hidden';
     const weather = await Weather.getCurrentWeather(input.value);
-    results.innerText = `You are in ${weather.location}, where it is ${weather.temperature}°c. I would describe the weather as - ${weather.description}`;
+    let newHtml = `<p class="locationAndTemp">${weather.location} - ${weather.temperature}°c</p>`;
+    newHtml += `<p class="description">${capitalizeString(
+      weather.description
+    )}`;
+    results.innerHTML = newHtml;
     const gif = await Gif.getGif(weather.description);
     gifLocationInDom.innerHTML = `<img src="${gif.url} class=\"gif\"" />`;
     card.style.visibility = 'visible';
@@ -23,3 +27,7 @@ submit.addEventListener('click', async () => {
     errorCard.style.visibility = 'visible';
   }
 });
+
+const capitalizeString = (string: string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
