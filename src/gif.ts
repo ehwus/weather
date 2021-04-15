9b51c847291f9ed14561d86c0fe63980d88f968b
Alex Withington-Smith <alex@awithsmith.com>
2021-04-15 14:40:56 +0100

    added random gif

--- src/gif.ts
+++ src/gif.ts
@@ -1,13 +1,16 @@
 import { Giphy } from './types';
 
+const numberOfPossibleGifs = 50;
+
 export default class Gif {
   static async getGif(query: string) {
     const response = await fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=RIQepIYMrVmr17DAq6yRyo6usx05E05n&q=${query}&limit=1&offset=0&rating=g&lang=en`,
+      `https://api.giphy.com/v1/gifs/search?api_key=RIQepIYMrVmr17DAq6yRyo6usx05E05n&q=${query}&limit=${numberOfPossibleGifs}&offset=0&rating=g&lang=en`,
       { mode: 'cors' }
     );
     const parsedResponse: Giphy = await response.json();
-    return new Gif(parsedResponse.data[0].images.original.url);
+    const randomImageIndex = Math.floor(Math.random() * 50);
+    return new Gif(parsedResponse.data[randomImageIndex].images.original.url);
   }
 
   constructor(public url: string) {}
